59b8835cddc366cb0cb406af71df9022a82e263c
Dmitriy <kaishien777@gmail.com>
2019-08-14 14:31:32 +0300

    Updates

---
+++ precache-manifest.8355f1b105609c252996fb45063c7f7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811e4846d742e39bab081a23a67d0102",
+    "url": "/vk_api_auth-react-redux/index.html"
+  },
+  {
+    "revision": "7579ce7b397e9f5f34f8",
+    "url": "/vk_api_auth-react-redux/static/css/main.f0cdf0f1.chunk.css"
+  },
+  {
+    "revision": "b5d192ab48c15a16dc57",
+    "url": "/vk_api_auth-react-redux/static/js/2.4f523fd4.chunk.js"
+  },
+  {
+    "revision": "7579ce7b397e9f5f34f8",
+    "url": "/vk_api_auth-react-redux/static/js/main.6a2ad2cf.chunk.js"
+  },
+  {
+    "revision": "77760b187ad302ba1443",
+    "url": "/vk_api_auth-react-redux/static/js/runtime~main.499427e2.js"
+  }
+]);
