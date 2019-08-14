f9c7cb0220e8c05c52958e8eff3c6d479ccd5304
Dmitriy <kaishien777@gmail.com>
2019-08-15 02:20:01 +0300

    Changed display friend list

--- app/src/components/FriendList/index.js
+++ app/src/components/FriendList/index.js
@@ -18,7 +18,6 @@ const FriendList = ({ friendList }) => (
             id={user.id}
             first_name={user.first_name}
             last_name={user.last_name}
-            photo={user.photo_200}
           />
         </li>
       ))}
